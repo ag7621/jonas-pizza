11286ca0d6122510f9c60cadb9030133e4e4fcc1
ag7621 <aaron.c.guerra@gmail.com>
2025-01-18 15:14:00 -0700

    moved assets, tested layout
    moved images to public folder, added basic components to test layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>:D</h1>
-    </>
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open! :D</footer>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="/assets/spinaci.bmp" alt="" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinich, and ricotta cheese</p>
+    </div>
   );
 }
 
